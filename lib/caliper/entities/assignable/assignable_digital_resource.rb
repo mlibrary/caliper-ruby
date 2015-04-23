5547dfb124b8dcf9f8750eead5864a9648861026
Anthony Whyte <anthwhyte@gmail.com>
2015-04-23 16:34:39 -0400

    Add license header, tidy up comments

--- lib/caliper/entities/assignable/assignable_digital_resource.rb
+++ lib/caliper/entities/assignable/assignable_digital_resource.rb
@@ -1,14 +1,26 @@
+##
+# This file is part of IMS Caliper Analytics™ and is licensed to
+# IMS Global Learning Consortium, Inc. (http://www.imsglobal.org)
+# under one or more contributor license agreements.  See the NOTICE
+# file distributed with this work for additional information.
+#
+# IMS Caliper is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# IMS Caliper is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with this program. If not, see http://www.gnu.org/licenses/.
+
 require_relative '../../entities/digital_resource'
 require_relative '../../entities/schemadotorg/thing'
 
 #
-#  @copyright @copyright ©2013 IMS Global Learning Consortium, Inc.  All Rights Reserved.
-#  @license For license information contact, info@imsglobal.org
+# AssignableDigitalResource entity. AssignableDigitalResource's prototype is set to DigitalResource.
 #
-# Represents AssignableDigitalResource.
-# AssignableDigitalResource's prototype set to DigitalResource
-#
-
 module Caliper
   module Entities
     module Assignable
--- lib/caliper/entities/digital_resource.rb
+++ lib/caliper/entities/digital_resource.rb
@@ -1,15 +1,26 @@
+##
+# This file is part of IMS Caliper Analytics™ and is licensed to
+# IMS Global Learning Consortium, Inc. (http://www.imsglobal.org)
+# under one or more contributor license agreements.  See the NOTICE
+# file distributed with this work for additional information.
+#
+# IMS Caliper is free software: you can redistribute it and/or modify it under
+# the terms of the GNU Lesser General Public License as published by the Free
+# Software Foundation, version 3 of the License.
+#
+# IMS Caliper is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License along
+# with this program. If not, see http://www.gnu.org/licenses/.
+
 require "json"
 require_relative 'entity'
 
 #
-#  
-#  @copyright @copyright ©2013 IMS Global Learning Consortium, Inc.  All Rights Reserved.
-#  @license For license information contact, info@imsglobal.org
-#
-# Public: Caliper representation of a CreativeWork (https://schema.org/CreativeWork)
-# We add learning specific attributes, including a list of
-# LearningObjective learning objectives and a list of
-# String keywords
+# Caliper representation of a CreativeWork. We add learning specific attributes, including a list of
+# LearningObjective learning objectives and a list of String keywords.  See https://schema.org/CreativeWork.
 #
 module Caliper
   module Entities
