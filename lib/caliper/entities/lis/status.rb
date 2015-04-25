8d89e93a4787f81f7edff4b2a24b70fba2382979
Anthony Whyte <anthwhyte@gmail.com>
2015-04-24 23:06:13 -0400

    Remove unnecessary require_relative

--- lib/caliper/entities/lis/status.rb
+++ lib/caliper/entities/lis/status.rb
@@ -15,8 +15,6 @@
 # You should have received a copy of the GNU Lesser General Public License along
 # with this program. If not, see http://www.gnu.org/licenses/.
 
-require_relative '../../entities/entity'
-require_relative '../../entities/foaf/agent.rb'
 require_relative '../../entities/jsonable'
 
 #
