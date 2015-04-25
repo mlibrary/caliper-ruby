d2d6ae0e718bdbbdfcc06d8c68d751cccf9878ce
Anthony Whyte <anthwhyte@gmail.com>
2015-04-24 23:24:13 -0400

    Fix require paths

--- lib/caliper/entities/response/multiple_choice_response.rb
+++ lib/caliper/entities/response/multiple_choice_response.rb
@@ -15,8 +15,8 @@
 # You should have received a copy of the GNU Lesser General Public License along
 # with this program. If not, see http://www.gnu.org/licenses/.
 
-require "require_all"
-require_all 'lib/caliper/entities/response/response_type.rb'
+require_relative './response'
+require_relative './response_type'
 
 #
 # Represents a response to a multiple choice question that permits a single option to be selected
--- lib/caliper/entities/response/response.rb
+++ lib/caliper/entities/response/response.rb
@@ -15,8 +15,8 @@
 # You should have received a copy of the GNU Lesser General Public License along
 # with this program. If not, see http://www.gnu.org/licenses/.
 
-require "require_all"
-require_all 'lib/caliper/entities/generatable.rb'
+require_relative '../entity'
+require_relative '../generatable'
 
 #
 # A Response
