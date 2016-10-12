be37a1c4d2d28207f519d9113b25e79c7ea647f6
Paul Kerschen <kerschen@berkeley.edu>
2016-10-12 10:43:23 -0700

    Remove EpubSubChapter. Resolves #91

--- lib/caliper/entities/digital_resource_type.rb
+++ lib/caliper/entities/digital_resource_type.rb
@@ -21,7 +21,6 @@ module Caliper
       ASSIGNABLE_DIGITAL_RESOURCE = "http://purl.imsglobal.org/caliper/v1/AssignableDigitalResource"
       EPUB_CHAPTER = "http://www.idpf.org/epub/vocab/structure/#chapter"
       EPUB_PART = "http://www.idpf.org/epub/vocab/structure/#part"
-      EPUB_SUB_CHAPTER = "http://www.idpf.org/epub/vocab/structure/#subchapter"
       EPUB_VOLUME = "http://www.idpf.org/epub/vocab/structure/#volume"
       FRAME = "http://purl.imsglobal.org/caliper/v1/Frame"
       MEDIA_LOCATION = "http://purl.imsglobal.org/caliper/v1/MediaLocation"
