d5ee24d63dbd43a0df1ec16bb2109ecf2b4c4f70
Paul Kerschen <kerschen@berkeley.edu>
2016-11-02 09:30:07 -0700

    Revert "Replace idpf IRIs with Caliper IRIs. Resolves #87"
    This reverts commit 2c0786b53ab016eb42e23f1ad3be8db9fb6b98bd.

--- lib/caliper/entities/digital_resource_type.rb
+++ lib/caliper/entities/digital_resource_type.rb
@@ -19,10 +19,10 @@ module Caliper
   module Entities
     module DigitalResourceType
       ASSIGNABLE_DIGITAL_RESOURCE = "http://purl.imsglobal.org/caliper/v1/AssignableDigitalResource"
-      EPUB_CHAPTER = "http://purl.imsglobal.org/caliper/v1/EpubChapter"
-      EPUB_PART = "http://purl.imsglobal.org/caliper/v1/EpubPart"
-      EPUB_SUB_CHAPTER = "http://purl.imsglobal.org/caliper/v1/EpubSubChapter"
-      EPUB_VOLUME = "http://purl.imsglobal.org/caliper/v1/EpubVolume"
+      EPUB_CHAPTER = "http://www.idpf.org/epub/vocab/structure/#chapter"
+      EPUB_PART = "http://www.idpf.org/epub/vocab/structure/#part"
+      EPUB_SUB_CHAPTER = "http://www.idpf.org/epub/vocab/structure/#subchapter"
+      EPUB_VOLUME = "http://www.idpf.org/epub/vocab/structure/#volume"
       FRAME = "http://purl.imsglobal.org/caliper/v1/Frame"
       MEDIA_LOCATION = "http://purl.imsglobal.org/caliper/v1/MediaLocation"
       MEDIA_OBJECT = "http://purl.imsglobal.org/caliper/v1/MediaObject"
